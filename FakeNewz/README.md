3fc070d0f3c11c95f43e105b9076bfffdb2d4f1a
antpowell <powell.anthony.e@gmail.com>
2018-07-23 16:00:26 -0500

    add routing module and added open flag to npm start script

--- FakeNewz/README.md
+++ FakeNewz/README.md
@@ -28,4 +28,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ## Team
 
-CAN
+Caroline, Anthony, N
--- FakeNewz/src/app/app.module.ts
+++ FakeNewz/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './/routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
