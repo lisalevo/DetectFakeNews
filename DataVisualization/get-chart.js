dd35f79497bf8b3be11f4277168e46a9847e3b24
Nathan Patnam <patnamnv@email.wofford.edu>
2018-07-24 13:15:04 -0700

    Deleted unecessary files

--- DataVisualization/get-chart.js
+++ DataVisualization/get-chart.js
@@ -1,6 +1,6 @@
 function search(tags) {
 
-  var url = "https://research.stlouisfed.org/ssi/search.php?q=";
+  var url = "https://fred.stlouisfed.org/search?st=";
   var i;
   for(i=0; i < tags.length; i++) {
       url += tags[i]
@@ -16,14 +16,20 @@ function getImage(url) {
 
   var request = require('request');
   var cheerio = require('cheerio');
-  var url = search(["unemployment", "usa"]);
-  let $ = cheerio.load(url);
+  var url = search(["unemployment", "rate"]);
+
 
   request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
-      var result = $('div[class="search-results"]').html();
-      console.log(result);
-      console.log(url);
+      let $ = cheerio.load(html);
+    var result = $('#series-pager > tbody > tr:nth-child(1) > td > div.col-xs-12.col-sm-10').html().trim();
+      //var result = $('#series-pager > tbody > tr:nth-child(1) > td > div.col-xs-12.col-sm-10').html();
+      //var graph_url = result.getElementsByTagName("a")[0].getAttribute("href")
+      //var resultJSON = JSON.parse(result);
+    var results = result.split(" ")[1]
+    var link = results.split('"')[1];
+    link = "https://fred.stlouisfed.org" + link
+    console.log(link);
     }
 
   });
