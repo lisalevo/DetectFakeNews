23be4f3a3f635ce167b1a075c3ea8d7f427ddb1e
Yingning Gui <yingning.gui@uwaterloo.ca>
2018-07-24 14:07:58 -0700

    puppet

--- DataVisualization/get-chart.js
+++ DataVisualization/get-chart.js
@@ -1,7 +1,8 @@
 function search(tags) {
 
-  var url = "https://research.stlouisfed.org/ssi/search.php?q=";
+  var url = "https://fred.stlouisfed.org/search?st=";
   var i;
+
   for(i=0; i < tags.length; i++) {
       url += tags[i]
 
@@ -16,19 +17,30 @@ function getImage(url) {
 
   var request = require('request');
   var cheerio = require('cheerio');
-  var url = search(["unemployment", "usa"]);
-  let $ = cheerio.load(url);
+  var url = search(["unemployment", "rate"]);
+
 
   request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
-      var result = $('div[class="search-results"]').html();
-      console.log(result);
-      console.log(url);
+      let $ = cheerio.load(html);
+    var result = $('#series-pager > tbody > tr:nth-child(1) > td > div.col-xs-12.col-sm-10').html().trim();
+    var results = result.split(" ")[1]
+    var link = results.split('"')[1];
+    link = "https://fred.stlouisfed.org" + link
+    console.log(link);
+    screenshot(link);
     }
-
-  });
-
+    });
 
 }
 
-getImage()
+const puppeteer = require('puppeteer');
+async function screenshot(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({path: 'image.png'});
+    await browser.close();
+}
+
+getImage();
