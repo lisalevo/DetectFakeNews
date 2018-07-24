3529476f70c8aee90bed5e58d3c634b90bd4b64a
Caroline Evans <evansjcaroline@gmail.com>
2018-07-23 18:41:48 -0700

    Basic UI implementation using Clarity Design System - Card framework and SCSS styling

--- FakeNewz/src/app/fact-checking-panel/fact-checking-panel.component.ts
+++ FakeNewz/src/app/fact-checking-panel/fact-checking-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fact-checking-panel',
   templateUrl: './fact-checking-panel.component.html',
-  styleUrls: ['./fact-checking-panel.component.css']
+  styleUrls: ['./fact-checking-panel.component.scss']
 })
 export class FactCheckingPanelComponent implements OnInit {
 
