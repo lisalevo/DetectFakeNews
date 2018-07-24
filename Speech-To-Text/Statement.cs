402e2ad81a382a178e8a1e2b1d95c8f52773a355
Mounika Chillamcherla <34459477+Mounika-Chillamcherla@users.noreply.github.com>
2018-07-24 15:48:38 -0700

    Speech to text output in json format

---
+++ Speech-To-Text/Statement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechToTextWPFSample
+{
+    public class Statement
+    {
+        public int id;
+
+        public string statementValue;
+
+        public long timeInSeconds;
+    }
+
+   public class Statements
+    {
+        public List<Statement> statements = new List<Statement>();
+    }
+}
