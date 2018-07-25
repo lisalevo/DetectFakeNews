e3622736063b439af2bbfc3a60f5cc66110d85a6
antpowell <powell.anthony.e@gmail.com>
2018-07-24 20:06:22 -0500

    adding observable to claims

--- FakeNewz/src/app/fact-checking-panel/fact-checking-panel.component.ts
+++ FakeNewz/src/app/fact-checking-panel/fact-checking-panel.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { FactCheckService } from '../services/fact-check.service';
+import { Observable } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-fact-checking-panel',
   templateUrl: './fact-checking-panel.component.html',
   styleUrls: ['./fact-checking-panel.component.scss'],
 })
-export class FactCheckingPanelComponent implements OnInit {
-  claims: Claim[] = [];
+export class FactCheckingPanelComponent implements OnInit, OnChanges {
+  claims: Observable<Claim[]>;
+  _claims: Claim[];
   constructor(private factCheckService: FactCheckService) {}
 
   ngOnInit() {
-    this.claims = this.factCheckService.getClaims();
+    this._claims = this.factCheckService.getClaims();
   }
 
-  checkTimeStap() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    // Add '${implements OnChanges}' to the class.
+
+    if (changes['claims'] && this.claims) {
+      this.claims.subscribe();
+    }
+  }
 }
