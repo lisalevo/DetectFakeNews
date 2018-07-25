1ac55ae8f7266aa364cbda576e1354a1ab523b5b
Caroline Evans <evansjcaroline@gmail.com>
2018-07-25 12:56:54 -0700

    timeStamp formatting into hr:min:sec

--- FakeNewz/src/app/timestamp/timestamp.component.ts
+++ FakeNewz/src/app/timestamp/timestamp.component.ts
@@ -7,8 +7,34 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TimestampComponent implements OnInit {
   @Input() timeStamp: number;
+  formattedTime: string;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.formattedTime = this.timeFormatter(this.timeStamp);
+
+  }
+
+  timeFormatter(time: number): string {
+    var hours = Math.floor(time / 3600);
+    time = time - hours * 3600;
+    var minutes = Math.floor(time / 60);
+    var seconds = time - minutes * 60;
+
+    if (hours > 0) {
+      var finalTime = hours.toString() + ':' + this.formatNumber(minutes) + ':' + this.formatNumber(seconds);
+    } else {
+      var finalTime = minutes.toString() + ':' + this.formatNumber(seconds);
+    }
+    return finalTime;
+  }
+
+  formatNumber(num: number): string {
+    var rtn = num.toString();
+    if (num < 10) {
+      rtn = "0" + rtn;
+    }
+    return rtn;
+  }
 }
