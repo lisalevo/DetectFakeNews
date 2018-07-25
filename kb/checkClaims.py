3d009532b965686c9e08208c93e28ac1972d1224
mirajp <mirage.patel@gmail.com>
2018-07-24 17:13:06 -0700

    Restructured

--- kb/checkClaims.py
+++ kb/checkClaims.py
@@ -2,7 +2,8 @@ import azure_query
 import article_analyzer
 import pprint
 
-matthew_claims = ["we are finally seeing rising wages",
+matthew_claims = [
+    "we are finally seeing rising wages",
     "African American unemployment stands at the lowest rate ever recorded",
     "and Hispanic American unemployment has also reached the lowest levels in history",
     "Small-business confidence is at an all-time high",
@@ -18,8 +19,9 @@ for claim in matthew_claims:
     snippetForClaim = article_analyzer.getMostSimilarSnippetFromArticles(claim, webResults[0:20]) # Take first 20
     pprint.pprint(snippetForClaim)
 
-"""
-maddys_claims = ['Since the election, we have created 2.4 million new jobs, including 200,000 new jobs in manufacturing alone',
+
+maddys_claims = [
+    'Since the election, we have created 2.4 million new jobs, including 200,000 new jobs in manufacturing alone',
     'After years and years of wage stagnation, we are finally seeing rising wages. Unemployment claims have hit a 45-year low',
     'The stock market has smashed one record after another, gaining $8 trillion and more in value in just this short amount of time',
     'Americans 401k, retirement, pension, and college-savings accounts have gone through the roof. And just as I promised the American people from this podium 11 months ago, we enacted the biggest tax cuts and reforms in American history. Our massive tax cuts provide tremendous relief for the middle class and small businesses. To lower tax rates for hardworking Americans, we nearly doubled the standard deduction for everyone',
@@ -31,4 +33,3 @@ for claim in maddys_claims:
     webResults = azure_query.getArticlesForQuery(claim)
     snippetForClaim = article_analyzer.getMostSimilarSnippetFromArticles(claim, webResults[0:10]) # Take first 10
     pprint.pprint(snippetForClaim)
-"""
